be50009fa3b7ff72b42c78782c2bc2718e213b7c
blsnowy3 <113197977+blsnowy3@users.noreply.github.com>
2022-09-10 16:45:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 
 import streamlit;
 import pandas;
+import requests;
 
 streamlit.title("hello world")
 
@@ -25,4 +26,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
