54d2904032006be8c2312a1c904a5a4a9ae29294
blsnowy3 <113197977+blsnowy3@users.noreply.github.com>
2022-09-10 21:22:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,11 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
  
-
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+    
 streamlit.title("hello world")
 
 streamlit.header("Breakfast Menu")
@@ -60,24 +64,24 @@ except URLError as e:
 
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+if streamlit.button("Get Fruit Load List"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    #my_cur = my_cnx.cursor()
 
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#my_data_row = my_cur.fetchone()
-#streamlit.text("Hello from Snowflake:")
-#streamlit.text(my_data_row)
+    #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+    #my_data_row = my_cur.fetchone()
+    #streamlit.text("Hello from Snowflake:")
+    #streamlit.text(my_data_row)
 
+    #my_cur.execute("select * from fruit_load_list")
+    #my_data_rows = my_cur.fetchone()
+    my_data_rows = get_fruit_load_list()
 
-my_cur.execute("select * from fruit_load_list")
-#my_data_rows = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
+    #streamlit.text("The fruit load list contains:")
+    #streamlit.text(my_data_rows)
 
-#streamlit.text("The fruit load list contains:")
-#streamlit.text(my_data_rows)
-
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+    streamlit.header("The fruit load list contains:")
+    streamlit.dataframe(my_data_rows)
 
 fruit_to_add = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thank you for adding ', fruit_to_add)
